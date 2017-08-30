348a016b467b42101ef5b246d8f62373a574c658
Евгений Мартынов <e.martynov@frontend.local>
2017-08-30 15:46:54 +0700

    Django 1.10 support

--- dbmail/urls.py
+++ dbmail/urls.py
@@ -2,9 +2,11 @@
 
 from django.conf.urls import url
 
+from .views import send_by_dbmail, mail_read_tracker
+
 
 urlpatterns = [
-    url(r'^api/', 'send_by_dbmail', name='db-mail-api'),
+    url(r'^api/', send_by_dbmail, name='db-mail-api'),
     url(r'^mail_read_tracker/(.*?)/$',
-        'mail_read_tracker', name='db-mail-tracker'),
+        mail_read_tracker, name='db-mail-tracker'),
 ]
